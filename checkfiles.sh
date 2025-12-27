b16568d8ccea1dba13cd66640047240c0865576d
PL4BL0 <49621941+PL4BL0@users.noreply.github.com>
2025-12-27 14:08:57 -0300

    Delete checkfiles.sh

--- checkfiles.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#ssh srvbackup@172.24.85.241 << EOF
-find /home/srvbackup/errores/autoriza/ -type f -mtime -1
-find /home/srvbackup/errores/online/ -type f -mtime -1
-find /home/srvbackup/errores/batch/ -type f -mtime -1 
-EOF
